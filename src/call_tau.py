5e4fbfc7dd4da72332e86e316897fafe0945770e
kame6821 <katherine.melbourne@colorado.edu>
2023-10-05 09:36:44 -0600

    made more basic notes on inputs and outputs

--- src/call_tau.py
+++ src/call_tau.py
@@ -1,1 +1,9 @@
 # This is where I will be writing the documentation for calling tau function
+import get_tau
+
+def call_tau():
+  concat_tau = []
+  for time_interval in ?:
+    one_tau = get_tau.get_tau()
+    concat_tau.append(one_tau)
+  return concat_tau
