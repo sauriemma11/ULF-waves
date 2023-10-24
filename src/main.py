b0ce8d3dbf4274cf5d3dfbafc08a64615deb2df8
aylaweitz <aylaweitz@cu-biot-13-10.203.176.165.int.colorado.edu>
2023-10-24 15:41:47 -0600

    implemented argparse + run.sh

--- src/main.py
+++ src/main.py
@@ -12,21 +12,44 @@ Main module
 import data_prep
 import mfa_transform
 import call_tau
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Pass in parameters for calculating Tau for a given dataset.'
+    )
+
+parser.add_argument('--filename',
+                    type=str,
+                    help='Path to file of interest.',
+                    required=True)
+
+parser.add_argument('--timespan',
+                    type=int,
+                    default=1,  # default is 1 hour
+                    help='Number of hours over which to find average Tau, PSD, and DLL values. Must be divisible by 24 (i.e. 1, 2, 3, 4, 6, 8, 12 hrs).',  #noqa
+                    required=False)
+
+parser.add_argument('--freq_band',
+                    type=list,
+                    default=[0.001, 0.01],  # default is 0.001 - 0.01 Hz
+                    help='Values to define the frequency band of interest. First element is lower frequency, and second element is upper frequency [Hz].',  #noqa
+                    required=False)
+
+args = parser.parse_args()
 
 
-def main(file_path, timespan, freq_band):
+def main(filename, timespan, freq_band):
     """
     Parameters
     ----------
-    file_path: str
+    filename: str
         data file to work with
 
     timespan: integer
         hours to split up data by 
 
     freq_band: list of 2 integers
-        first element is lower frequency
-        second element is upper frequency
+        first element is lower frequency, and second element is upper frequency
 
     Returns
     -------
@@ -34,7 +57,7 @@ def main(file_path, timespan, freq_band):
 
     """
     # prep data
-    variable_dict = data_prep.read_nc_file(file_path) # dictionary of all variables
+    variable_dict = data_prep.read_nc_file(filename) # dictionary of all variables
 
     # mfa_transform
     mfa_dict = mfa_transform.main(variable_dict)
@@ -48,5 +71,4 @@ def main(file_path, timespan, freq_band):
 
 
 if __name__ == '__main__':
-    # Do the main stuff
-    # main()
+    main(args.filename, args.timespan, args.freq_band)
