6d5b97ef0772317957a6a7007b4501f94291d443
aylaweitz <aylaweitz@cu-biot-13-10.203.176.165.int.colorado.edu>
2023-10-24 15:57:52 -0600

    stylized main

--- src/main.py
+++ src/main.py
@@ -26,13 +26,15 @@ parser.add_argument('--filename',
 parser.add_argument('--timespan',
                     type=int,
                     default=1,  # default is 1 hour
-                    help='Number of hours over which to find average Tau, PSD, and DLL values. Must be divisible by 24 (i.e. 1, 2, 3, 4, 6, 8, 12 hrs).',  #noqa
+                    help="""Number of hours over which to find average Tau, PSD, and DLL values.
+                      Must be divisible by 24 (i.e. 1, 2, 3, 4, 6, 8, 12 hrs).""",
                     required=False)
 
 parser.add_argument('--freq_band',
                     type=list,
                     default=[0.001, 0.01],  # default is 0.001 - 0.01 Hz
-                    help='Values to define the frequency band of interest. First element is lower frequency, and second element is upper frequency [Hz].',  #noqa
+                    help="""Values to define the frequency band of interest.
+                      First element is lower frequency, and second element is upper frequency [Hz].""",
                     required=False)
 
 args = parser.parse_args()
@@ -55,16 +57,13 @@ def main(filename, timespan, freq_band):
     tau_dict: dict
 
     """
-    # prep data
     variable_dict = data_prep.read_nc_file(filename) # dictionary of all variables
 
-    # mfa_transform
     mfa_dict = mfa_transform.main(variable_dict)
 
-    # calc Tau
-    tau_dict = call_tau.call_tau(mfa_dict, timespan,
-                                 freq_band)  # calculates tau for each time
-    # window
+    tau_dict = call_tau.call_tau(mfa_dict,
+                                 timespan,
+                                 freq_band)
 
     # CREATE PLOTS???
 
