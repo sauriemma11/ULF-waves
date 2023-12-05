840f26c9cdb557dc7c9361a58513b3a967ba618f
aspenmag <aspen.daivs@colorado.edu>
2023-12-04 19:09:31 -0700

    plotting unit test updates

--- test/unit/test_plots.py
+++ test/unit/test_plots.py
@@ -10,16 +10,19 @@ class TestPlots(unittest.TestCase):
 
     def test_plot_data(self):
         # Test random input data
-        t_hp_z = np.linspace(0, 10, 100)
-        f_hp_z = np.linspace(0, 0.05, 100)
-        sgdb_hp_z = np.random.rand(100, 100) * 40 - 20
-        dt_g16 = np.linspace(0, 10, 100)
-        highps_z_all = np.random.uniform(-10, 10, len(dt_g16))
+        dt_g16 = np.linspace(0, 24, 864000)
+        avg_psd = np.linspace(0, 1000, 864000)
+        t_hp_z = np.linspace(0, 24, 864000)
+        avg_tau = np.linspace(0, 1000, 24)
+        window_start_time = np.linspace(0, 24, 864000)
+        highps_z_all = np.random.uniform(-10, 10, 864000)
 
-        try:
-            plot_data(t_hp_z, f_hp_z, sgdb_hp_z, dt_g16, highps_z_all)
-        except Exception as e:
-            self.fail(f"plot_data raised an exception: {e}")
+        # try:
+        plot_data(t_hp_z, highps_z_all, window_start_time, avg_psd,
+                  avg_tau)
+
+        # except Exception as e:
+        #     self.fail(f"plot_data raised an exception: {e}")
 
     def test_plot_data_inputs(self):
         # Test if inputs are non-array
